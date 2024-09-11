8ce704acb6012b22ef09a9eca87ad03ead035617
Vatsal Rishabh <108565564+vatsalrishabh@users.noreply.github.com>
2024-09-12 05:05:38 +0530

    Update BaseUrl.jsx

--- src/Components/BaseUrl.jsx
+++ src/Components/BaseUrl.jsx
@@ -1,4 +1,4 @@
 // BaseUrl.jsx
 // export const BaseUrl = "http://localhost:3000/";
 
-export const BaseUrl = "https://college-crush-backend.onrender.com/:10000/";
+export const BaseUrl = "https://college-crush-backend.onrender.com:10000/";
